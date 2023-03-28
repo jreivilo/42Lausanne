68a7fb52537e34eddd8b5abedcdae34545cc48c2
jreivilo <j.reivilo@outlook.com>
2023-03-28 13:17:04 +0200

    cpp/cpp02

---
+++ cpp/cpp02/ex00/Fixed.cpp
@@ -0,0 +1,42 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+	std::cout << "Default constructor called" << std::endl;
+	this->_rawBits = 0;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed::Fixed( Fixed const & src )
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = src;
+}
+
+int Fixed::getRawBits( void ) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->_rawBits);
+}
+
+void Fixed::setRawBits( int const raw )
+{
+	this->_rawBits = raw;
+}
+
+Fixed & Fixed::operator=( Fixed const & rhs )
+{
+	std::cout << "Assignation operator called" << std::endl;
+	this->_rawBits = rhs.getRawBits();
+	return *this;
+}
+
+
+
+//explain polymorphism in C++ and how it is used in the context of classes and inheritance
+// polymorphism is the ability of an object to take on many forms. The most common use of polymorphism in OOP occurs when a parent class reference is used to refer to a child class object.
+// 
---
+++ cpp/cpp02/ex00/Fixed.hpp
@@ -0,0 +1,30 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <string>
+
+
+class Fixed
+{
+	private:
+		int					_rawBits;
+		static const int	_fractionalBits = 8;
+	public:
+		Fixed();
+		~Fixed();
+		Fixed( Fixed const & src ); //copy constructor
+		Fixed & operator=( Fixed const & rhs ); //assignment operator overload (assignation operator) 
+		int		getRawBits( void ) const;
+		void	setRawBits( int const raw );
+};
+
+
+//whats of a copy constructor?
+//copy constructor is a constructor that creates an object by initializing it with an object of the same class, which has been created previously. The copy constructor is used to:
+//initialize one object from another of the same type
+
+//explain assignment operator
+//The assignment operator is used to copy the value of one object to another already initialized object of the same type. It is a member function which is called when the = operator is used with the object as left operand.
+
+#endif
