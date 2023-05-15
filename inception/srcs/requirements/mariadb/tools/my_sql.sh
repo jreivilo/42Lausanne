a1bdc4087780deb6d765c1c7f6e509367509cabc
jreivilo <j.reivilo@outlook.com>
2023-05-15 19:05:28 +0200

    inception

---
+++ inception/srcs/requirements/mariadb/tools/my_sql.sh
@@ -0,0 +1,8 @@
+service mysql start # start mysql
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;" #
+mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO '${SQL_USER}'@'%' WITH GRANT OPTION;"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql -e "FLUSH PRIVILEGES;"
+mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+exec mysqld_safe
