8d45fa0d7fae9e41c5a1556c6ccbbacfb7f70c08
jreivilo <j.reivilo@outlook.com>
2023-06-26 12:52:39 +0200

    mhh

--- inception/srcs/requirements/mariadb/tools/my_sql.sh
+++ inception/srcs/requirements/mariadb/tools/my_sql.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
-
+rm -rf /var/lib/mysql/$SQL_DATABASE
 if [ ! -d "/var/lib/mysql/$SQL_DATABASE" ]; then
-	mysql_install_db --user=root --datadir=/var/lib/mysql
+	# mysql_install_db --user=root --datadir=/var/lib/mysql
     service mysql start
 
     echo "$SQL_DATABASE database does not exist"
+	echo "$SQL_DATABASE creation ..."
     mysql -u root -e "CREATE DATABASE $SQL_DATABASE;"
+	echo 
     mysql -u root -e "CREATE USER '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';"
     mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '$SQL_USER'@'%';"
 	mysql -u root -e "FLUSH PRIVILEGES;"
     mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD';"
-	mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 
-	mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}' WITH GRANT OPTION;"
-	mysql -u root -e "FLUSH PRIVILEGES;"
-	mysqladmin -u root -p $SQL_ROOT_PASSWORD shutdown
+	# mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+	# mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}' WITH GRANT OPTION;"
+	# mysql -u root -e "FLUSH PRIVILEGES;"
+	# mysqladmin -u root -p $SQL_ROOT_PASSWORD shutdown
+	# service mysql stop
 fi
 
 # mysqld_safe
